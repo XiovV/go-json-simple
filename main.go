2fa777996dae6fb635a05cc31916fd7e0491b34e
xiovv <xiovv@tutanota.com>
2019-04-03 22:42:14 +0200

    wrong files

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type SensorReading struct {
+	Name     string `json:"name"`
+	Capacity int    `json:capacity`
+	Time     string `json:time`
+}
+
+func main() {
+	fmt.Println("vim-go")
+
+	jsonString := `{"name": "battery sensor", "capacity": 40, "time": "2019-01-21T19:07:28Z"}`
+
+	var reading SensorReading
+	err := json.Unmarshal([]byte(jsonString), &reading)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%+v\n", reading.Capacity)
+
+}
